a482aabaef28be95e7ea09da77aa3f32a728a54a
maulana adam <wandgenius6@gmail.com>
2024-09-07 11:55:19 +0700

    removing websocket

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,6 @@ import bodyParser from "body-parser";
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import http from 'http';
-import { Server } from 'socket.io';
 
 // IMPORT CONTROLLERS
 import login from "./Controllers/authController";
@@ -12,20 +11,19 @@ import voteController from './Controllers/voteController';
 import paslonController from './Controllers/PaslonController';
 import feedback from './Controllers/feedbackController';
 // import paslonModel from './Models/PaslonModel';
-import PaslonModel from "./Models/PaslonModel";
+// import PaslonModel from "./Models/PaslonModel";
 
 const port = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 
-const io = new Server(server, {
-    cors: {
-        origin: 'http://localhost:5173', // Replace with your frontend's origin or keep '*' for all origins
-        methods: ['GET', 'POST', 'DELETE', 'PUT'],
-        credentials: true,
-        
-    }
-});
+// const io = new Server(server, {
+//     cors: {
+//         origin: 'http://localhost:5173', // Replace with your frontend's origin or keep '*' for all origins
+//         methods: ['GET', 'POST', 'DELETE', 'PUT'],
+//         credentials: true,
+//     }
+// });
 
 app.use(bodyParser.json());
 app.use(cors({
@@ -35,7 +33,7 @@ app.use(cors({
     
 }));
 app.use(cookieParser());
-
+app.use('/uploads', express.static(__dirname + '/uploads'))
 // Setup your routes
 app.get("/", (req, res) => {
     res.json({
@@ -51,47 +49,47 @@ app.use("/api", paslonController);
 app.use("/api", feedback);
 
 // Setup Socket.io
-const paslon = new PaslonModel();
+// const paslon = new PaslonModel();
 
-interface PaslonData {
-    id?: number;
-    nomor_urut: string;
-    nama: string;
-    caksis: string;
-    cawaksis: string;
-    visi: string;
-    misi: string;
-    program_kerja: string;
-    img: string;
-    total?: number; 
-}
+// interface PaslonData {
+//     id?: number;
+//     nomor_urut: string;
+//     nama: string;
+//     caksis: string;
+//     cawaksis: string;
+//     visi: string;
+//     misi: string;
+//     program_kerja: string;
+//     img: string;
+//     total?: number; 
+// }
 
-io.on('connection', (socket) => {
-    console.log('User connected:', socket.id);
+// io.on('connection', (socket) => {
+//     console.log('User connected:', socket.id);
 
-    const emitPaslonData = async () => {
-        try {
-            const paslons = await paslon.All() as PaslonData[];
-            socket.emit('updatePaslonData', paslons);
-        } catch (err: any) {
-            socket.emit('error', {
-                message: 'Failed to retrieve paslon data',
-                error: err.message
-            });
-        }
-    };
+//     const emitPaslonData = async () => {
+//         try {
+//             const paslons = await paslon.All() as PaslonData[];
+//             socket.emit('updatePaslonData', paslons);
+//         } catch (err: any) {
+//             socket.emit('error', {
+//                 message: 'Failed to retrieve paslon data',
+//                 error: err.message
+//             });
+//         }
+//     };
 
 
-    emitPaslonData();
+//     emitPaslonData();
 
     
-    const intervalId = setInterval(emitPaslonData, 60000);
+//     const intervalId = setInterval(emitPaslonData, 60000);
 
-    socket.on('disconnect', () => {
-        console.log('User disconnected:', socket.id);
-        clearInterval(intervalId);
-    });
-});
+//     socket.on('disconnect', () => {
+//         console.log('User disconnected:', socket.id);
+//         clearInterval(intervalId);
+//     });
+// });
 
 // Start the server
 server.listen(port, () => {
